1abb2fa00e2fc9f7fb44caf0bf6000b6a40fa55b
Emilvinther <95082486+Emilvinther@users.noreply.github.com>
2023-04-18 15:50:47 +0200

    Add files via upload

---
+++ ThreadSynchronization-master/DiningPhilosophers/Philosopher.cs
@@ -0,0 +1,100 @@
+﻿using System.ComponentModel;
+
+namespace DiningPhilosophers;
+
+public class Philosopher
+{
+    public int Id;
+    private int _rightFork;
+    private int _leftFork;
+
+    //It makes sure the philosophers get the right forks
+    public Philosopher(int id )
+    {
+        Id = id;
+        if(id == 0)
+        {
+            _rightFork = 0;
+            _leftFork = 4;
+        }
+        else 
+        {
+            _rightFork = Id;
+            _leftFork = Id-1;
+        }
+    }
+    // Runs the state of the philosophers
+    public void Do(object callback)
+    {
+        while (true)
+        {
+            Think();
+            Wait();
+        }
+    }
+
+    // If there is no forks available, it will run this code and monitor how many cycles it goes through without eating
+    private void Wait()
+    {
+        int tries = 0;
+        bool ate = false;
+        Console.WriteLine("Philosopher{0} is waiting", Id);
+        while (!ate)
+        {
+            try
+            {
+                if (Monitor.TryEnter(Program.forks[_rightFork]))
+                {
+                    if (Monitor.TryEnter(Program.forks[_leftFork]))
+                    {
+                        Console.WriteLine("Philosopher{0} tried {1} times before eating", Id, tries);
+                        tries = 0;
+                        ate = true;
+                        Eat();
+                        Monitor.Exit(Program.forks[_leftFork]);
+                    }
+                    Monitor.Exit(Program.forks[_rightFork]);
+                }
+                tries++;
+                
+            }
+            finally
+            {
+
+                if(tries == 100)
+                {
+                    Dead();
+                }
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+
+    // Abort to kill a thread
+
+    private void Dead()
+    {
+        Console.WriteLine("Philosopher{0} is DEAD", Id);
+        while (true)
+        {
+            //Thread.Abort is deprecated
+        }
+    }
+
+    // Eating with a randomized value
+    private void Eat()
+    {
+        Console.WriteLine("Philosopher{0} is eating", Id);
+        Random r = new Random();
+        Thread.Sleep(r.Next(500, 2000));
+    }
+    // Think with a randomized value
+    private void Think()
+    {
+        Console.WriteLine("Philosopher{0} is thinking", Id);
+        Random r = new Random();
+        Thread.Sleep(r.Next(500, 2000));
+
+    }
+}
---
+++ ThreadSynchronization-master/DiningPhilosophers/Program.cs
@@ -0,0 +1,23 @@
+﻿using DiningPhilosophers;
+
+public class Program
+{
+    // Makes a list as an objective that holds objectives.
+    public static Fork[] forks = new Fork[5] { new Fork(), new Fork(), new Fork(), new Fork(),  new Fork() };
+    // Making Philosopher object
+    static Philosopher[] Philosophers = new Philosopher[5];
+    public static void Main()
+    {
+        // Creats the Pilosophers in a loop and create threats with 1 philosopher each
+        for (int i = 0; i < 5; i++)
+        {
+            Philosopher phil = new Philosopher(i);
+            Philosophers[i] = phil;
+        }
+        for (int i = 0; i < Philosophers.Length; i++)
+        {
+            ThreadPool.QueueUserWorkItem(Philosophers[i].Do);
+        }
+        Console.Read();
+    }
+}
